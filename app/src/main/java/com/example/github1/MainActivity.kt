d074501e474afe45cce2cdb4bd53cef2130ad23f
shahzaib38 <crystalbusinessapps@gmail.com>
2023-10-25 19:06:18 +0400

    Master Branch Commit 3

--- app/src/main/java/com/example/github1/MainActivity.kt
+++ app/src/main/java/com/example/github1/MainActivity.kt
@@ -30,12 +30,10 @@ class MainActivity : AppCompatActivity() {
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+                .setAction("Action", null).show() }
 
 
         println("Master Branch")
-        println("Shahzaib Branch")
 
 
 
